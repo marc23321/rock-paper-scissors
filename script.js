730fc495b811defd7d50c6ec895bd193d81807a1
marcowapo1738 <jomarc233@gmail.com>
2023-12-21 14:36:01 -0600

    Add generator and compare function to each evenet listening instance

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// let names = ["rock","paper","scissors"];
+
 // let images = document.querySelectorAll('.clickableImage');
 
 // images.forEach(function(image) {
@@ -36,15 +36,42 @@
 const rock = document.getElementById('rockImage')
 const paper = document.getElementById('paperImage')
 const scissors = document.getElementById('scissorsImage')
+let names = ["rock","paper","scissors"];
+let userInput;
+let playerScore;
+let computerScore;
+let drawScore;
+
+function generator(){
+    let randomName = Math.floor(Math.random() * names.length);
+    let result = names[randomName];
+    return result;
+ }
 
 rock.addEventListener('click',e => {
-    console.log("rock")
+    let userInput = "rock";
+    let result = generator();
+    compareResults(result, userInput);
 })
 
 paper.addEventListener('click', e =>{
-    console.log("paper")
+    let userInput = "paper";
+    let result = generator();
+    compareResults(result, userInput)
 })
 
 scissors.addEventListener('click', e=>{
-    console.log("Scissors")
+    let userInput = "scissors";
+    let result = generator();
+    compareResults(result, userInput);
 })
+
+
+
+function compareResults(result, userInput){
+        if(userInput == result){
+            console.log("Its a Match");
+      }else{
+           console.log("No this time");
+    }
+ }
