5c3adc19daf5b21f2edeed4a9ddc66382baece30
marcowapo1738 <jomarc233@gmail.com>
2023-12-22 14:28:58 -0600

    Un refactored javascript code with Rock Paper Scissors Logic

--- script.js
+++ script.js
@@ -37,10 +37,9 @@ const rock = document.getElementById('rockImage')
 const paper = document.getElementById('paperImage')
 const scissors = document.getElementById('scissorsImage')
 let names = ["rock","paper","scissors"];
-let userInput;
-let playerScore;
-let computerScore;
-let drawScore;
+let playerScore = 0;
+let computerScore =0;
+let drawScore = 0;
 
 function generator(){
     let randomName = Math.floor(Math.random() * names.length);
@@ -57,7 +56,8 @@ rock.addEventListener('click',e => {
 paper.addEventListener('click', e =>{
     let userInput = "paper";
     let result = generator();
-    compareResults(result, userInput)
+    compareResults(result, userInput);
+
 })
 
 scissors.addEventListener('click', e=>{
@@ -66,12 +66,79 @@ scissors.addEventListener('click', e=>{
     compareResults(result, userInput);
 })
 
+// function gamePlay(count){
+//     while(count < 5 ){
+//         compareResults(result, userInput)
+//     }
+//     count  = count +1;
 
+// }
 
 function compareResults(result, userInput){
-        if(userInput == result){
-            console.log("Its a Match");
-      }else{
-           console.log("No this time");
-    }
- }
+            if(userInput == result){
+                    console.log("Its a Match");
+                    drawScore = drawScore  + 1; // or any other value
+                    document.getElementById('drawscoreValue').textContent = drawScore;
+            }else if(userInput == "rock" && result == "scissors"){
+                    console.log("You Won");
+                    playerScore = playerScore + 1 ; // or any other value
+                    document.getElementById('playerscoreValue').textContent = playerScore;
+            }else if(userInput == 'paper' && result == 'scissors'){
+                    console.log("You Lost");
+                    computerScore = computerScore + 1 ; // or any other value
+                    document.getElementById('computerscoreValue').textContent = computerScore;
+            }else if(userInput == 'paper' && result == 'rock'){
+                    console.log("You Won");
+                    playerScore = playerScore + 1 ; // or any other value
+                    document.getElementById('playerscoreValue').textContent = playerScore;
+            }else if(userInput == "rock" && result == "paper"){
+                    console.log("You Lost");
+                    computerScore = computerScore + 1 ; // or any other value
+                    document.getElementById('computerscoreValue').textContent = computerScore;
+                }else if (userInput == 'scissors' && result == 'paper'){
+                    console.log("You Won");
+                    playerScore = playerScore + 1 ; // or any other value
+                    document.getElementById('playerscoreValue').textContent = playerScore;
+                }else if (userInput == 'scissors' && result == 'rock'){
+                    console.log("You Lost");
+                    computerScore = computerScore + 1 ; // or any other value
+                    document.getElementById('computerscoreValue').textContent = computerScore;
+                }else{
+                console.log(userInput);
+                console.log(result);
+            }
+            if (computerScore == 5 || playerScore == 5) {
+                if (computerScore == 5){
+                    let LoseCopy = "You Lost This Round!n"
+                    console.log(computerScore)
+                    console.log(playerScore)
+                    document.getElementById('finalresult').textContent = LoseCopy;
+                    resetScores();
+
+                }else{
+                    let WinCopy = "You Won this Round!"
+                    console.log(computerScore)
+                    console.log(playerScore)
+                    document.getElementById('finalresult').textContent = WinCopy;
+                    resetScores();
+                    document.getElementById('finalresults').textContent =''
+                }s
+            }
+ } 
+
+
+ function resetScores() {
+    // Reset scores after someone reaches 5 points
+    playerScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+
+    document.getElementById('playerscoreValue').textContent = playerScore;
+    document.getElementById('computerscoreValue').textContent = computerScore;
+    document.getElementById('drawscoreValue').textContent = drawScore;
+}
+
+
+function resetMessage(){
+    
+}
